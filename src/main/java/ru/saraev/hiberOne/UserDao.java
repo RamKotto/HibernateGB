dfdefed1a2c0888ce3792592685f7ef791b48ffb
d.saraev <black8.tabls@gmail.com>
2022-10-13 15:12:55 +0300

    lesson one

---
+++ src/main/java/ru/saraev/hiberOne/UserDao.java
@@ -0,0 +1,17 @@
+package ru.saraev.hiberOne;
+
+import java.util.List;
+
+public interface UserDao {
+    User findById(Long id);
+
+    List<User> findAll();
+
+    User findByName(String name);
+
+    void save(User user);
+
+    void update(Long id, String name);
+
+    void testCache();
+}
